c50eafaf6c3b7c30ea050bd6d496082454efa013
Colin Davitian <colin@kyocare.com>
2025-11-13 19:14:36 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3687,3 +3687,4 @@ app.listen(PORT, '0.0.0.0', async () => {
     console.log(`📱 Access your chat at: http://localhost:${PORT}`);
     console.log(`🔐 AWS Cognito authentication enabled for kyocare.com domain`);
 });
+// this change is inserted to test git pushes - please delete later
