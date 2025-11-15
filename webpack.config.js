18c0a3a6b84007c6d4e4c1258a8a2a19bbdf045b
Colin Davitian <colin@kyocare.com>
2025-11-14 16:41:03 -0800

    Update dist/index.html, homepage.html, src/index.html and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
     chunkFilename: '[name].chunk.js',
+    publicPath: '/',
     clean: true,
   },
   module: {
